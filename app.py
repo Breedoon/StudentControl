7519c09f5eb7e43b766b271029083a2c7f97b77f
Breedoon <danir@programmer.net>
2018-09-19 14:38:22 -0500

    3

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
+from flask_jsglue import JSGlue
 
 app = Flask(__name__)
-
+jsglue = JSGlue(app)
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def index():
+    if session.__contains__('user_id'):
+        return render_template('dashboard.html')
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
